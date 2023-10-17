ac0d04061767ae91efba12bbf6b7ff9ba25d67e3
Mikey Murphy <n00221455@iadt.ie>
2023-10-17 16:44:41 +0100

    Laravel Breeze Authentication

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,42 +1,57 @@
-@extends('layouts.app')
+@extends('layouts.myapp')
 
 @section('content')
 <h2>Todos</h2>
 <a href="{{route('todos.create')}}"> Create</a>
-<table>
-    <thead>
-        <tr>
-            <th>Title</th>
-            <th>Snippet</th>
-            <th>Created</th>
-            <th>misc</th>
-        </tr>
-    </thead>
-    <tbody>
 
-        @forelse($todos as $todo)
-        <tr>
-            <td>
-                {{$todo->title}}
-            </td>
-            <td>
-                {{$todo->body}}
-            </td>
-            <td>
-                {{$todo->created_at}}
-            </td>
-            <td>
-                <a href="{{ route('todos.show', $todo->id)}}">Read More</a>
-            </td>
-        </tr>
+
+
+
+<div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Title
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Snippit
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Created
+                </th>
+                
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($todos as $todo)
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{$todo->title}}
+                </th>
+                <td class="px-6 py-4">
+                    {{$todo->body}}
+                </td>
+                <td class="px-6 py-4">
+                    {{$todo->created_at}}
+                </td>
+                <td class="px-6 py-4">
+                    <a href="{{ route('todos.show', $todo->id)}}">Read More</a>
+                </td>
+            </tr>
+        
 
         @empty
         <h4>No Todos found!</h4>
         @endforelse
-        
-        
-        
-    </tbody>
+            
+            
+        </tbody>
+    </table>
+</div>
 
-</table>
+
+
+
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TodoController;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,9 +15,20 @@ use App\Http\Controllers\TodoController;
 |
 */
 
-//Route::get('/', function () {
-//    return view('todos.index');
-//});
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [TodoController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::resource('todos', TodoController::class);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
